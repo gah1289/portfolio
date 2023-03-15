91456d8c208b8bb66b93cc22ee95267eeb25519e
GabbyMcCarthy <gah1289@gmail.com>
2023-03-15 13:28:47 -0400

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import React, { useState, useRef, useEffect } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyRoutes from './Routes';
-import NavBar from './NavBar/NavBar';
 
 function App() {
 	return (
 		<div className="App">
-			{/* <NavBar /> */}
 			<MyRoutes />
 		</div>
 	);
